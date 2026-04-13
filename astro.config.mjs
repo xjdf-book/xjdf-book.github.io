b81a41f507a05a5c77eba7df2c83d45822cb8251
Dr. Stefan Meissner <stefan.meissner@ricebean.net>
2026-04-13 22:38:40 +0200

    restructuring

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,17 +12,51 @@ export default defineConfig({
 			expressiveCode: {
 				plugins: [pluginLineNumbers()],
 			},
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						async: true,
+						src: 'https://www.googletagmanager.com/gtag/js?id=G-PKSFYWL6HW',
+					},
+				},
+				{
+					tag: 'script',
+					content: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-PKSFYWL6HW');
+					`,
+				},
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/xjdf-book/xjdf-book.github.io' }],
 			sidebar: [
 				{
-					label: 'XJDF Book',
+					label: 'Introduction',
 					items: [
-						{ label: 'Introduction', slug: 'introduction/introduction' },
-						{ label: 'Major Changes in XJDF', slug: 'major-changes/major-changes' },
-						{ label: 'XJDF Essentials', slug: 'xjdf-essentials/xjdf-essentials' },
-						{ label: 'Resources', slug: 'resources/resources' },
-						{ label: 'Extensibility', slug: 'extensibility/extensibility' },
-						{ label: 'Identifiers', slug: 'identifiers/identifiers' }
+						{ label: 'Introduction', slug: 'chap_01_introduction/introduction' }
+					],
+				},
+				{
+					label: 'Major Changes in XJDF',
+					items: [
+						{ label: 'Major Changes in XJDF', slug: 'chap_02_major-changes/major-changes' }
+					],
+				},
+				{
+					label: 'XJDF Essentials',
+					items: [
+						{ label: 'XJDF Essentials', slug: 'chap_03_xjdf-essentials/xjdf-essentials' },
+						{ label: 'Resources', slug: 'chap_03_xjdf-essentials/resources' },
+						{ label: 'Extensibility', slug: 'chap_03_xjdf-essentials/extensibility' },
+						{ label: 'Identifiers', slug: 'chap_03_xjdf-essentials/identifiers' },
+					],
+				},
+				{
+					label: 'Product Description',
+					items: [
+						{ label: 'Product Description', slug: 'chap_04_product-description/product-description' }
 					],
 				}
 			],
